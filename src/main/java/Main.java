924a71acc703448f25b17e44df01b659b4dc2cac
seanjungmsba <seanjeong1996@gmail.com>
2022-07-27 12:44:00 -0400

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.Scanner;
+import java.util.concurrent.*;
 
 public class Main {
     public static int countFinishedFutures(List<Future> futures) {
@@ -16,31 +15,39 @@ public class Main {
         return counter;
     }
 
+    public static Callable<String> getCallableString() {
+        Callable<String> callableString = () -> {
+            System.out.println(Thread.currentThread().getName() + " is responsible for this call");
+            StringBuilder sb = new StringBuilder("Example-String");
+            return sb.toString();
+        };
+        return callableString;
+    }
 
+    public static Callable<Integer> getCallableInteger() {
+        // your code here
+        Callable<Integer> callableInteger = () -> {
+            int exampleNumber = 2;
+            return exampleNumber;
+        };
+        return callableInteger;
+    }
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        Future<Integer> future = new SquareCalculator().calculate(10);
-
-        while(!future.isDone()) {
-            System.out.println("Calculating...");
-            Thread.sleep(300);
-        }
-
-        Integer result = future.get();
-        System.out.println(result);
-
-//        countFinishedFutures();
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        Future<String> stringFuture = singleThreadExecutor.submit(getCallableString());
+        System.out.println("RESULT: String future returns " + stringFuture.get());
+        Future<Integer> integerFuture = singleThreadExecutor.submit(getCallableInteger());
+        System.out.println("RESULT: Integer future returns " + integerFuture.get());
+        List<Future> futureList = new ArrayList<>();
+        System.out.println("STATUS: adding stringFuture to the list");
+        futureList.add(stringFuture);
+        System.out.println("STATUS: adding integerFuture to the list");
+        futureList.add(integerFuture);
+        System.out.println("Number of completed Future object ===> " + countFinishedFutures(futureList));
+        singleThreadExecutor.shutdown();
     }
 
 }
 
-class SquareCalculator {
-    private ExecutorService executor = Executors.newSingleThreadExecutor();
 
-    public Future calculate(Integer input) {
-        return executor.submit(() -> {
-            Thread.sleep(1000);
-            return input * input;
-        });
-    }
-}
