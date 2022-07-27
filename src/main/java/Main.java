c99818d28002c6e703411f724b809211e9080b64
seanjungmsba <seanjeong1996@gmail.com>
2022-07-27 16:00:36 -0400

    Implemented two versions of countFinishedFutures()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.*;
+import java.util.stream.IntStream;
 
 public class Main {
-    public static int countFinishedFutures(List<Future> futures) {
-        // your code here
+
+    /* counting number of finished futures using iterative approach */
+    public static int countFinishedFuturesVersionOne(List<Future> futures) {
         int counter = 0;
         for (Future future: futures) {
             if (future.isDone()) {
@@ -13,6 +14,11 @@ public class Main {
             }
         }
         return counter;
+    }
+
+    /* counting number of finished futures using stream API */
+    public static int countFinishedFuturesVersionTwo(List<Future> futures) {
+        return (int)futures.stream().filter(future -> future.isDone()).count();
     }
 
     public static Callable<String> getCallableString() {
@@ -27,27 +33,44 @@ public class Main {
     public static Callable<Integer> getCallableInteger() {
         // your code here
         Callable<Integer> callableInteger = () -> {
+            System.out.println(Thread.currentThread().getName() + " is responsible for this call");
             int exampleNumber = 2;
             return exampleNumber;
         };
         return callableInteger;
     }
 
+    public static Callable<Double> getCallableDouble() {
+        return () -> {
+            System.out.println(Thread.currentThread().getName() + " is responsible for this call");
+            Double doubleNum = Math.random();
+            Double roundedDoubleNum = Math.round(doubleNum * 100.0) / 100.0;
+            return roundedDoubleNum;
+        };
+    }
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
-        Future<String> stringFuture = singleThreadExecutor.submit(getCallableString());
-        System.out.println("RESULT: String future returns " + stringFuture.get());
-        Future<Integer> integerFuture = singleThreadExecutor.submit(getCallableInteger());
-        System.out.println("RESULT: Integer future returns " + integerFuture.get());
+
+        final int THREAD_CNT = 5;
+        ExecutorService executor = Executors.newFixedThreadPool(THREAD_CNT);
+
+        Future<String> stringFuture = executor.submit(getCallableString());
+        System.out.println("RESULT 1: String future returns ===> " + stringFuture.get());
+        Future<Integer> integerFuture = executor.submit(getCallableInteger());
+        System.out.println("RESULT 2: Integer future returns ===> " + integerFuture.get());
+        Future<Double> doubleFuture = executor.submit(getCallableDouble());
+        System.out.println("RESULT 3: Double future returns ===> " + doubleFuture.get());
+
         List<Future> futureList = new ArrayList<>();
-        System.out.println("STATUS: adding stringFuture to the list");
-        futureList.add(stringFuture);
-        System.out.println("STATUS: adding integerFuture to the list");
-        futureList.add(integerFuture);
-        System.out.println("Number of completed Future object ===> " + countFinishedFutures(futureList));
-        singleThreadExecutor.shutdown();
+        IntStream.rangeClosed(1, THREAD_CNT).forEach(i -> {
+            futureList.add(stringFuture);
+            futureList.add(integerFuture);
+            futureList.add(doubleFuture);
+        });
+
+        System.out.println("Number of finished Future object (using iterative approach) ===> " + countFinishedFuturesVersionOne(futureList));
+        System.out.println("Number of finished Future object (using stream API) ===> " + countFinishedFuturesVersionTwo(futureList));
+        executor.shutdown();
     }
 
 }
-
-
